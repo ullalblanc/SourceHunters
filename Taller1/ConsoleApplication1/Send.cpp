fde2d9fc3b2914cb3a0cc4eb5381fc796f04582e
ullalblanc <dani.s.sintas@gmail.com>
2017-02-16 16:32:30 +0100

    Send

--- Taller1/ConsoleApplication1/Send.cpp
+++ Taller1/ConsoleApplication1/Send.cpp
@@ -9,4 +9,16 @@ Send::Send()
 
 Send::~Send()
 {
+
 }
+
+void Send::SendMessages()
+{
+	
+
+	if (send->send(mensajes,mensajes->size()) != sf::Socket::Done)
+	{
+		std::cout << "Error al enviar" << mensajes << std::endl;
+	}
+}
+
--- Taller1/ConsoleApplication1/Send.h
+++ Taller1/ConsoleApplication1/Send.h
@@ -1,8 +1,18 @@
 #pragma once
+
+#include <SFML\Graphics.hpp>
+#include <SFML\Network.hpp>
+#include <iostream>
+
 class Send
 {
 public:
 	Send();
 	~Send();
+
+	void SendMessages();
+
+	sf::TcpSocket *send;
+	std::string *mensajes;
 };
 
