76eed2053656e754097a80ffd234bb4ff9747900
Pol <poljan95@gmail.com>
2017-03-16 11:53:13 +0100

    mas server

--- Practica1/ChatLib/Game.h
+++ Practica1/ChatLib/Game.h
@@ -11,7 +11,24 @@ struct Player {
 	int _num;
 	std::string _name;
 	int _score = 0;
-	std::string _word = "";
+	int _answer = 0;
+	//std::string _word = "";
 	std::vector<std::string> _log;
 	
+};
+
+class Timer {
+	std::clock_t time = std::clock();
+public:
+	Timer() {};
+	~Timer() {};
+
+	void Start() {
+		time = std::clock() + MAXTIME;
+	};
+
+	bool Check() {
+		if (time < std::clock()) return true;
+		else return false;
+	};
 };
--- Practica1/Server/Server.cpp
+++ Practica1/Server/Server.cpp
@@ -49,7 +49,7 @@ void cleanPlayers(int* playerChecks) {
 }
 
 void sendAll(Send* sender, std::vector<sf::TcpSocket*> sockets) { // per misatges iguals que s'envien a tots el jugadors
-	for (int i = 0; i < MAX_USERS; i++)
+	for (int i = 0; i < MAX_USERS; i++)                           // podriam ficar un bool de parametre si volem que es faci blocking
 	{
 		sender->send = sockets[i];
 		sender->SendMessages();
@@ -64,11 +64,14 @@ int main()
 	std::vector<sf::TcpSocket*> sockets;
 	sf::TcpSocket* sockettmp = new sf::TcpSocket;
 	MessageManager protocol;
+	Timer timer;
 	
 	// Crear players per guardar la info
 	std::vector<Player> player(MAX_USERS);
-	player[0]._num = 0;
-	player[1]._num = 1;
+	for (int i = 0; i < MAX_USERS; i++)
+	{
+		player[i]._num = i;
+	}
 	int playerChecks[MAX_USERS]; // per comprobar quins usuaris hem vist ja
 	cleanPlayers(playerChecks);
 	std::string textConsole = "Connected to: ";
@@ -91,41 +94,58 @@ int main()
 		return -1;
 	}
 	//Accept per els dos jugadors
-	if (listener.accept(*sockettmp) != sf::Socket::Done) {
-		std::cout << "Error al acceptar conexió" << std::endl;
-		return -1;
+
+	for (int i = 0; i < MAX_USERS; i++)
+	{
+		if (listener.accept(*sockettmp) != sf::Socket::Done) {
+			std::cout << "Error al acceptar conexió" << std::endl;
+			return -1;
+		}
+		std::cout << "\n New user" << std::endl;
+		sockettmp->setBlocking(false);
+		sockets.push_back(sockettmp);
+		sockettmp = new sf::TcpSocket;
 	}
-	std::cout << "\n New user" << std::endl;
-	sockettmp->setBlocking(false);
-	sockets.push_back(sockettmp);
-	sockettmp = new sf::TcpSocket;
-	if (listener.accept(*sockettmp) != sf::Socket::Done) {
+
+	
+	/*if (listener.accept(*sockettmp) != sf::Socket::Done) {
 		std::cout << "Error al acceptar conexió" << std::endl;
 		return -1;
 	}
 	listener.close();
 	std::cout << "\n New user" << std::endl;
 	sockettmp->setBlocking(false);
-	sockets.push_back(sockettmp);
+	sockets.push_back(sockettmp);*/
 	
 	//Rebre noms dels jugadors
-	int num = 1;
-	while (num != 3) {
+	int num = 0;
+	while (num != 4) {
 		for (int i = 0; i < player.size(); i++)
 		{
 			receiver.socket = sockets[i];
 			if (receiver.ReceiveMessages()) {
 				player[i]._name = protocol.GetWord(command);
-				sender.send = sockets[i];
+				/*sender.send = sockets[i];
 				command = protocol.CreateMessage(4,0, player[i]._num,player[i]._name);
-				sender.SendMessages();
+				sender.SendMessages();*/
 				num++;
 			}
 		}
 	}
 
+	for (int i = 0; i < MAX_USERS; i++)
+	{
+		sockets[i]->setBlocking(true);
+		sender.send = sockets[i];
+		for (int j = 0; j < MAX_USERS; j++)
+		{
+			command = protocol.CreateMessage(4, 0, player[j]._num, player[j]._name);
+			sender.SendMessages();
+		}
+		sockets[i]->setBlocking(false);
+	}
 	//sockets[0]->setBlocking(true);
-	sender.send = sockets[0];
+	/*sender.send = sockets[0];
 	command = protocol.CreateMessage(4, 0, player[1]._num, player[1]._name);
 	sender.SendMessages();
 	//sockets[0]->setBlocking(false);
@@ -134,7 +154,7 @@ int main()
 	sender.send = sockets[1];
 	command = protocol.CreateMessage(4, 0, player[0]._num, player[0]._name);
 	sender.SendMessages();
-	//sockets[1]->setBlocking(false);
+	//sockets[1]->setBlocking(false);*/
 
 	// OPEN CHAT WINDOW
 	std::vector<std::string> aMensajes;
@@ -142,7 +162,7 @@ int main()
 	std::string mensaje = "";
 	//sender.mensajes = &mensaje;
 	bool serverOn = true;
-	int points = 3;
+	int answerOrder = 1;
 
 	while (serverOn)
 	{
@@ -156,29 +176,59 @@ int main()
 			{
 				//sender.send = &
 			}*/
-			sender.SendMessages();
+			//sender.SendMessages();
 			state = play;
+			timer.Start(); // començar timer
 			break;
 		case play:
 			// TODO: rebre resposta dels jugadors i enviarles
-			for (int i = 0; i < sockets.size(); i++)
-			{
-				receiver.socket = sockets[i];
-				if (receiver.ReceiveMessages()) {
-					playerChecks[i] = 1;
-					for (int j = 0; j < sockets.size(); j++)
-					{
-						sender.send = sockets[j];					
-						sender.SendMessages();
+			if (timer.Check()) { // si sha acabat el temps
+				for (int i = 0; i < sockets.size(); i++)
+				{
+					receiver.socket = sockets[i];
+					if (receiver.ReceiveMessages()) {
+						playerChecks[i] = answerOrder; // per asignar el odre de respondre
+						answerOrder++;
+						//player[i]._answer = protocol.GetWord(); // TODO: mirar de aconseguir la paraula com a int
+						//command = protocol.CreateMessage(); // TODO: misatge de que jugador i ha respos x
+						sendAll(&sender, sockets); // enviar a tots la resposta que ha donat el jugador i
 					}
+					//sender.send = &
 				}
-				//sender.send = &
+				if (answerOrder == 5) { // tots han respost dins del temps
+					state = points;
+					answerOrder = 1;
+				}	
 			}
-			// TODO: si tots responen dins del temps, actualitzar puntuacions
-			// TODO: si s'acaba el temps, tornar a enviar paraula
+			else { 
+				state = points;
+				answerOrder = 1;
+			}
 			break;
 		case points:
 			// TODO: actualitzar les puntuacions de la pregunta i enviar les puntuacions. Si un te 5 punts, acabar partida
+			while (answerOrder != 5) { // canviar per evitar locuras
+				for (int i = 0; i < MAX_USERS; i++)
+				{
+					if (playerChecks[i] == answerOrder) {
+						// TODO: comprobar si la resposta es correcta i sumarli els punts
+
+						answerOrder++;
+						// TODO: crear misatge de la puntuacio del jugador i, i enviarla a tots
+						// command = protocol.CreateMessage();
+						sendAll(&sender, sockets);
+					}
+				}
+			}
+			cleanPlayers(playerChecks);
+			
+			for (int i = 0; i < MAX_USERS; i++)
+			{
+				if (player[i]._score > 10) {
+					state = win;
+				}
+			}
+			if (state != win) state = send;
 			break;
 		case win:
 			// TODO: acabar partida.
